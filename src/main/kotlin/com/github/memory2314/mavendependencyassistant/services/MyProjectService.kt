7121a67afb2f80536d6b14ec08ad4eb9d02ae369
GitHub Action <action@github.com>
2026-08-03 05:18:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/memory2314/mavendependencyassistant/services/MyProjectService.kt
+++ src/main/kotlin/com/github/memory2314/mavendependencyassistant/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.memory2314.mavendependencyassistant.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.memory2314.mavendependencyassistant.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
