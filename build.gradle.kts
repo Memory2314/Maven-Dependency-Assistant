a3a4c70f3f440f4b4c0e822ca33e1ae86b7292cc
Memory2314 <smqy2314@gmail.com>
2026-08-03 15:00:06 +0800

    build: add compatibility support for IntelliJ IDEA 2023.3+

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        intellijIdea("2025.3.5")
+        intellijIdea("2023.3")
         testFramework(TestFrameworkType.Platform)
     }
 }
